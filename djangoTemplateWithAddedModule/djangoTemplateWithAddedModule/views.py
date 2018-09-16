6f2e2a2e904c24ec58b3a0aebacfb049240b2580
Christopher Adams <chrismichaeladams@gmail.com>
2018-09-16 15:50:04 -0400

    Dog photos loading w/ captions and info

--- djangoTemplateWithAddedModule/djangoTemplateWithAddedModule/views.py
+++ djangoTemplateWithAddedModule/djangoTemplateWithAddedModule/views.py
@@ -5,27 +5,13 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from utils.scraper import *
 from utils.parse_scraped import *
+from json import dumps
+import json
 
 
 def home(request):
-    #return HttpResponse("Hello world!")
-    return render(request, 'home.html')
-
-def about(request):
     url = 'https://toolkit.rescuegroups.org/j/3/list3_layout.php?petfocus_0=204&location_0=&distance_0=&resultSort_0=animalName&resultOrder_0=asc&page_0=1&searchString_0=&action_0=search&animalID=undefined&toolkitIndex=0&toolkitKey=nxhKP5s7'
     raw_html = simple_get(url)
-
     url_parsed = parse_html(raw_html)
 
-    return render(
-        request, 'about.html',
-        {'lenUrl': len(raw_html), 'rawHtml': raw_html, 'urlParsed': url_parsed}
-    )
-
-
-
-
-
-
-
-
+    return HttpResponse(json.dumps(url_parsed), content_type='application/json')
--- reactApp/src/app.js
+++ reactApp/src/app.js
@@ -1,15 +1,42 @@
-var app = {
-  title: 'Python Animal Adoption Scraper',
-  subtitle: 'Scraping aint scrapping...'
+class App extends React.Component {
+    constructor() {
+        super();
+
+        this.state = {
+            dogs: []
+        }
+    };
+
+    componentDidMount() {
+        fetch('http://localhost:8000')
+            .then(result => result.json())
+            .then(dogs => this.setState({dogs: dogs}))
+    }
+
+    render() {
+        return (
+            <div className="container2">
+                <div className="container1">
+                    {
+                        this.state.dogs.map(dog => 
+                            <div key={dog.image}>
+                                <h1>{dog.Name}</h1>
+                                <img src={dog.image} />
+                                <p><b>Age: </b> {dog['age:']}</p>
+                                <p><b>Breed: </b> {dog['breed:']}</p>
+                                <p><b>Gender: </b> {dog['gender:']}</p>
+                                <p><b>Adoption Fee: </b> {dog['fee:']}</p>
+                                <p><b>Last Updated: </b> {dog['lastUpdated:']}</p>
+                                <br />
+                                <hr />
+                            </div>
+                        )
+                    }
+                </div>
+            </div>
+        );
+    }
 }
 
-var template = (
-  <div>
-    <h1>{app.title}</h1>
-    <p>{app.subtitle}</p>
-    <p>Locations: Providence, RI</p>
-  </div>
-);
-
 var appRoot = document.getElementById('app');
-ReactDOM.render(template, appRoot);
+ReactDOM.render(<App />, appRoot);
