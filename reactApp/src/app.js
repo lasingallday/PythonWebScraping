62684d643fab8edec05b380d897bbc51094e2c9d
Christopher Adams <chrismichaeladams@gmail.com>
2018-09-22 15:53:54 -0400

    Convert to reusable component and update title

--- reactApp/src/app.js
+++ reactApp/src/app.js
@@ -1,41 +1,59 @@
 class App extends React.Component {
     constructor() {
         super();
-
-        this.state = {
-            dogs: []
-        }
     };
 
     componentDidMount() {
-        fetch('http://localhost:8000')
-            .then(result => result.json())
-            .then(dogs => this.setState({dogs: dogs}))
+        document.title = 'Python Animal Adoption Scraper';
     }
 
     render() {
+        const url = 'http://localhost:8000';
         return (
-            <div className="container2">
-                <div className="container1">
-                    {
-                        this.state.dogs.map(dog => 
-                            <div key={dog.image}>
-                                <h1>{dog.Name}</h1>
-                                <img src={dog.image} />
-                                <p><b>Age: </b> {dog['age:']}</p>
-                                <p><b>Breed: </b> {dog['breed:']}</p>
-                                <p><b>Gender: </b> {dog['gender:']}</p>
-                                <p><b>Adoption Fee: </b> {dog['fee:']}</p>
-                                <p><b>Last Updated: </b> {dog['lastUpdated:']}</p>
-                                <br />
-                                <hr />
-                            </div>
-                        )
-                    }
-                </div>
-            </div>
+          <FetchAPI apiUrl={url} />
         );
     }
+}
+
+/* Component for Fetching data from an API */
+class FetchAPI extends React.Component {
+  constructor() {
+      super();
+
+      this.state = {
+          data: []
+      }
+  };
+
+  componentDidMount() {
+      fetch(this.props.apiUrl)
+          .then(result => result.json())
+          .then(data => this.setState({data: data}))
+  }
+
+  render() {
+    return (
+        <div className="container2">
+            <div className="container1">
+                {
+                    this.state.data.map(dog => 
+                        <div key={dog.image}>
+                            <h1>{dog.Name}</h1>
+                            <img src={dog.image} />
+                            <p><b>Age: </b> {dog['age:']}</p>
+                            <p><b>Breed: </b> {dog['breed:']}</p>
+                            <p><b>Gender: </b> {dog['gender:']}</p>
+                            <p><b>Adoption Fee: </b> {dog['fee:']}</p>
+                            <p><b>Last Updated: </b> {dog['lastUpdated:']}</p>
+                            <br />
+                            <hr />
+                        </div>
+                    )
+                }
+            </div>
+        </div>
+    );
+  }
 }
 
 var appRoot = document.getElementById('app');
